e790a0bca5753bbe3083d9777ce35c807cc57f51
Nazeer <vipsalmansuperman@gmail.com>
2024-02-18 12:13:57 +0530

    Movies Listing Page changes

---
+++ src/app/movies/layout.tsx
@@ -0,0 +1,22 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Movies List",
+  description: "Book movies near you",
+};
+
+export default function MoviesLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <Grid container>
+      <Grid item xs={2}></Grid>
+      <Grid item xs={10}>
+        {children}
+      </Grid>
+      <Grid item xs={2}></Grid>
+    </Grid>
+  );
+}
